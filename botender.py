ed7a7e34dd89a340ffdfb07e9a723da7dc815f95
Till Grutschus <till.grutschus@outlook.com>
2023-11-22 17:34:11 +0100

    added logging

---
+++ botender.py
@@ -0,0 +1,70 @@
+"""Startup file for botender."""
+
+import argparse
+import datetime
+import logging
+import os
+import sys
+
+import colorlog
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Botender")
+
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Enable debug mode",
+    )
+
+    return parser.parse_args()
+
+
+def setup_logging(debug: bool) -> None:
+    log_format = (
+        "%(asctime)s - "
+        "%(log_color)s%(levelname)s%(reset)s - "
+        "%(module)s - %(message)s"
+    )
+    log_colors_config = {
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red",
+    }
+
+    colorlog_formatter = colorlog.ColoredFormatter(
+        log_format,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        reset=True,
+        log_colors=log_colors_config,
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(colorlog_formatter)
+
+    os.makedirs("logs", exist_ok=True)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_filename = f"logs/botender-{timestamp}.log"
+    file_handler = logging.FileHandler(log_filename)
+    file_handler.setFormatter(colorlog_formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(console_handler)
+    root_logger.addHandler(file_handler)
+
+    if debug:
+        root_logger.setLevel(logging.DEBUG)
+    else:
+        root_logger.setLevel(logging.INFO)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    setup_logging(args.debug)
+
+    logger.info("Starting botender...")

e5eb3b6435f69ea5fd025611a7a91386c3d5b785
Till Grutschus <till.grutschus@outlook.com>
2023-12-05 15:38:20 +0100

    removed manual setting of gaze state in interaction manager

--- botender/interaction/interaction_manager.py
+++ botender/interaction/interaction_manager.py
@@ -43,7 +43,12 @@ class InteractionManagerThread(Thread):
         self._webcam_processor = webcam_processor
         self._furhat = FurhatRemoteAPI(furhat_remote_address)
         self._gaze_coordinator = GazeCoordinatorThread(
-            self._furhat, self._perception_manager, self._webcam_processor, frame_width, frame_height, number_of_cells_per_side
+            self._furhat,
+            self._perception_manager,
+            self._webcam_processor,
+            frame_width,
+            frame_height,
+            number_of_cells_per_side,
         )
 
         logger.debug("Spawning GazeCoordinatorThread...")
@@ -89,7 +94,6 @@ class InteractionManagerThread(Thread):
         logger.info("Started...")
         while not self._stopped:
             if self._should_start_interaction():
-                self._gaze_coordinator.set_gaze_state(GazeClasses.FACE)
                 self._start_interaction()
             else:
                 self._gaze_coordinator.set_gaze_state(GazeClasses.IDLE)
